2695c2ef6398a6de9fc1afcdf482496bd61dbd23
AlastorCarmesi <carmesiblog7@gmail.com>
2023-05-20 11:22:05 -0600

    Codigo

---
+++ fire.js
@@ -0,0 +1,15 @@
+var firebase = require('firebase')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDiLnrTW9q6cCqFektC-HpxQw65Y3ZCVQ8",
+    authDomain: "diabolos-security-system.firebaseapp.com",
+    databaseURL: "https://diabolos-security-system-default-rtdb.firebaseio.com",
+    projectId: "diabolos-security-system",
+    storageBucket: "diabolos-security-system.appspot.com",
+    messagingSenderId: "828860406671",
+    appId: "1:828860406671:web:754021ad65f1fec99e89fa",
+    measurementId: "G-BTPB0XBY5B"
+  };
+
+const fire = firebase.initializeApp(firebaseConfig);
+module.exports = fire;
