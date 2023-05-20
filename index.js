1cc45a1ed834a415eca2c6df25054f9deefa792a
AlastorCarmesi <carmesiblog7@gmail.com>
2023-05-20 13:44:40 -0600

    Codigo

--- index.js
+++ index.js
@@ -13,7 +13,27 @@ app.get('/', (req, res) => {
     res.send(
       '<h1>API Express & Firebase MonitoreO2</h1><ul><li><p><b>GET /ver</b></p></li><li><p><b>GET /valor</b></p></li><li><p><b>GET /estado</b></p></li><li><p><b>POST /insertar</b>  => {temp, hum, gas, ruido, nombre, fecha}</p></li><li><p><b>POST /encender</b></p></li><li><p><b>POST /apagar</b></p></li><li><p>/encender</p></li><li><p>/apagar</p></li><li><p>/estado</p></li></ul>')
   })
-  
+
+  app.get('/ver', (req, res) => {
+    const db = fire.firestore();
+      db.settings({
+        timestampsInSnapshots: true
+      });
+      var wholeData = []
+      db.collection('/BD').orderBy('Fecha', 'ID').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+        
+          wholeData.push(doc.data())
+        });
+        console.log(wholeData)
+        res.send(wholeData)
+      })
+      .catch(error => {
+        console.log('Error!', error);
+    })
+  })
+
   app.listen(PORT, () => {
     console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
   });
