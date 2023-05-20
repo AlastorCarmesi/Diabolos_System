041d81cd42ff2ec70ed1fcb6ed029652ca30041b
AlastorCarmesi <carmesiblog7@gmail.com>
2023-05-20 12:55:54 -0600

    Codigo

--- fire.js
+++ fire.js
@@ -1,26 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-import { initializeApp } from 'firebase/app';
-require('firebase/database')
 const firebaseConfig = {
+  apiKey: "AIzaSyDiLnrTW9q6cCqFektC-HpxQw65Y3ZCVQ8",
+  authDomain: "diabolos-security-system.firebaseapp.com",
+  databaseURL: "https://diabolos-security-system-default-rtdb.firebaseio.com",
+  projectId: "diabolos-security-system",
+  storageBucket: "diabolos-security-system.appspot.com",
+  messagingSenderId: "828860406671",
+  appId: "1:828860406671:web:754021ad65f1fec99e89fa",
+  measurementId: "G-BTPB0XBY5B"
+};
 
-    apiKey: "AIzaSyDiLnrTW9q6cCqFektC-HpxQw65Y3ZCVQ8",
-  
-    authDomain: "diabolos-security-system.firebaseapp.com",
-  
-    databaseURL: "https://diabolos-security-system-default-rtdb.firebaseio.com",
-  
-    projectId: "diabolos-security-system",
-  
-    storageBucket: "diabolos-security-system.appspot.com",
-  
-    messagingSenderId: "828860406671",
-  
-    appId: "1:828860406671:web:754021ad65f1fec99e89fa",
-  
-    measurementId: "G-BTPB0XBY5B"
-  
-  };
-
-const app = firebase.initializeApp(firebaseConfig);
-const database = getDatabase(app);
-export default database;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
