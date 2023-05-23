62b8b022ab0f5edceac6ec0c3ea6cb2dd4020b8b
AlastorCarmesi <carmesiblog7@gmail.com>
2023-05-22 19:46:24 -0600

    Codigo

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ app.get('/', (req, res) => {
 
   app.get('/ver', (req, res) => {
     const db = fire.firestore();
-    db.settings({
-      timestampsInSnapshots: true
-    });
+   
       var wholeData = []
       db.collection('/BD').orderBy('ID', 'Sensor_PIR').get()
       .then(snapshot => {
